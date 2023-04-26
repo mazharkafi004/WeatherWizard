36c63b0975a000db9ab18723dd35c72d81953144
mazharkafi004 <mazharkafi53@gmail.com>
2023-04-26 18:52:29 +0600

    responsive

--- main.js
+++ main.js
@@ -154,55 +154,55 @@ function getWeather() {
         weatherDataElement.innerHTML = `
         <div id="card1" class="card">
         <div class="card-header">
-  <div>
-    <p>Current Weather of:<i class="fa fa-map-marker" aria-hidden="true"></i> <b>${name}, ${country} </b></p>
-  </div>
-  <div class="time">
-    <span class="muted-text"><small><i class="fa fa-clock-o" aria-hidden="true"></i> Last updated at: ${localTime.toLocaleTimeString(
-      [],
-      timeOptions
-    )} local time</small></span>
-  </div>
-</div>
-        <hr>
+        <div>
+        <p>Current Weather of:<i class="fa fa-map-marker" aria-hidden="true"></i> <b>${name}, ${country} </b></p>
+        </div>
+        <div class="time">
+        <span class="muted-text"><small><i class="fa fa-clock-o" aria-hidden="true"></i> Last updated at: ${localTime.toLocaleTimeString(
+          [],
+          timeOptions
+          )}<br>local time</small></span>
+          </div>
+          </div>
+          <hr>
         <img src="http://openweathermap.org/img/wn/${icon}.png" alt="${description}" style="width: 10%"> 
         <br>
         <div class="container">
         <p>${description} </p>
-<br>
+        <br>
         </div>
-      </div>
+        </div>
         <div id="card2" class="card">
         <div class="container">
         
         <hr class="solid">
         <div class="row">
-  <div class="column">
-  <p><i class="fa fa-thermometer-empty" aria-hidden="true"></i> Temperature: ${temp}°C</p>
-  <p><i class="fa fa-registered" aria-hidden="true"></i> Real feel: ${feels_like}°C</p>
-  <p><i class="fa fa-compress" aria-hidden="true"></i> Pressure: ${pressure} mb</p>
-  <p><i class="fa fa-tint" aria-hidden="true"></i> Humidity: ${humidity}%</p>
-  <p><i class="fa fa-eye" aria-hidden="true"></i> Visibility: ${visible}km</p>
-  </div>
-  <div class="column">
-  <p><i class="fa fa-superpowers" aria-hidden="true"></i> Wind speed: ${speed} m/s</p>
-  <p><i class="fa fa-arrows" aria-hidden="true"></i> Wind direction: ${deg}° ${windDirection}</p>
-  <p><i class="fa fa-cloud" aria-hidden="true"></i> Cloudiness: ${all}%</p>
-  <p><span class="material-symbols-outlined" style="font-size: 18px">
-  water_lux
-  </span> Sunrise:
-  </span></i> ${new Date(sunrise * 1000).toLocaleTimeString()}</p>
-  <p><span class="material-symbols-outlined" style="font-size: 18px">
-  wb_twilight
-  </span> Sunset: ${new Date(sunset * 1000).toLocaleTimeString()}</p></div>
-</div>
+        <div class="column">
+          <p><i class="fa fa-thermometer-empty" aria-hidden="true"></i> Temperature: ${temp}°C</p>
+          <p><i class="fa fa-registered" aria-hidden="true"></i> Real feel: ${feels_like}°C</p>
+          <p><i class="fa fa-compress" aria-hidden="true"></i> Air Pressure: ${pressure} mb</p>
+          <p><i class="fa fa-tint" aria-hidden="true"></i> Humidity: ${humidity}%</p>
+          <p><i class="fa fa-eye" aria-hidden="true"></i> Visibility: ${visible}km</p>
+        </div>
+          <div class="column">
+          <p><i class="fa fa-superpowers" aria-hidden="true"></i> Wind speed: ${speed} m/s</p>
+          <p><i class="fa fa-arrows" aria-hidden="true"></i> Wind direction: ${deg}° ${windDirection}</p>
+          <p><i class="fa fa-cloud" aria-hidden="true"></i> Cloudiness: ${all}%</p>
+          <p><span class="material-symbols-outlined" style="font-size: 18px">
+          water_lux
+          </span> Sunrise:
+          </span></i> ${new Date(sunrise * 1000).toLocaleTimeString()}</p>
+          <p><span class="material-symbols-outlined" style="font-size: 18px">
+          wb_twilight
+          </span> Sunset: ${new Date(sunset * 1000).toLocaleTimeString()}</p></div>
+        </div>
 
   
         </div>
         <br>
         <hr>
         <button class="button" onclick="show()">See another</button>
-      </div>
+        </div>
   
       `;
         let checkbox = document.getElementById("myCheckbox");
